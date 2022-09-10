b5a7a281805de1541f48281283a906fc4b482208
sebastianmonten <94540990+sebastianmonten@users.noreply.github>
2022-09-10 18:34:17 +0200

    Create first idea for implementing library

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,3 +1,53 @@
 fn main() {
     println!("Hello, world!");
+    let p0 = Piece {
+        kind: PieceKind::Pawn,
+        color: Color::White,
+    };
+    dbg!(p0);
+}
+
+/*
+Gcode:
+f2l1 = forward 2, left 1
+f-   = forward until other piece or edge
+fl-   = diag forward/left until other piece or edge
+_f2l1 = forward 2, left 1 ONLY IF occupied by other player
+*f2l1 = forward 2, left 1 ONLY IF first move
+
+
+EXAMPLE: Pawn Gcode:
+f1
+*f2
+_fl1
+_fr1
+
+EXAMPLE: Bishop Gcode:
+fl-
+fr-
+bl-
+br-
+
+*/
+
+
+#[derive(Debug)]
+enum Color {
+    Black,
+    White,
+}
+#[derive(Debug)]
+enum PieceKind {
+    Pawn,
+    Bishop,
+    Knight,
+    Rook,
+    Queen,
+    King,
+}
+
+#[derive(Debug)]
+struct Piece {
+    kind: PieceKind,
+    color: Color,
 }
